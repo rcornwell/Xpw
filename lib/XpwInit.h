3ccba74bf0523088032435f32bc93ffe2b03e0ce
Richard Cornwell <rcornwell@github.com>
1997-10-08 17:11:50 +0000

    Initial revision

---
+++ lib/XpwInit.h
@@ -0,0 +1,42 @@
+/*
+ * XpwInialize widget set.
+ *
+ * Copyright 1997 Richard P. Cornwell All Rights Reserved,
+ *
+ * The software is provided "as is", without warranty of any kind, express
+ * or implied, including but not limited to the warranties of
+ * merchantability, fitness for a particular purpose and non-infringement.
+ * In no event shall Richard Cornwell be liable for any claim, damages
+ * or other liability, whether in an action of contract, tort or otherwise,
+ * arising from, out of or in connection with the software or the use or other
+ * dealings in the software.
+ *
+ * Permission to use, copy, and distribute this software and its
+ * documentation for non commercial use is hereby granted,
+ * provided that the above copyright notice appear in all copies and that
+ * both that copyright notice and this permission notice appear in
+ * supporting documentation.
+ *
+ * The sale, resale, or use of this library for profit without the
+ * express written consent of the author Richard Cornwell is forbidden.
+ * Please see attached License file for information about using this
+ * library in commercial applications, or for commercial software distribution.
+ *
+ */
+
+/*
+ * $Id$
+ *
+ * $Log:$
+ *
+ */
+#ifndef _Init_H_
+#define _Init_H_
+
+_XFUNCPROTOBEGIN
+
+extern void XpwInitializeWidgetSet();	/* called from ClassInit procs */
+
+_XFUNCPROTOEND
+
+#endif /*_Init_H_*/

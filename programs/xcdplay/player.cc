de9178c9024972118c046162801914600607261a
Richard Cornwell <rcornwell@github.com>
1998-01-26 01:02:02 +0000

    Support for Stacker CDrom drives.

--- programs/xcdplay/player.cc
+++ programs/xcdplay/player.cc
@@ -3,12 +3,15 @@
  *
  * Code to handle main interface panel.
  *
- * $Log: $
+ * $Log: player.cc,v $
+ * Revision 1.1  1997/12/16 05:48:46  rich
+ * Initial revision
+ *
  *
  */
 
 #ifndef lint
-static char        *rcsid = "$Id: $";
+static char        *rcsid = "$Id: player.cc,v 1.1 1997/12/16 05:48:46 rich Exp rich $";
 #endif
     
 /* System stuff */
@@ -23,6 +26,7 @@ static char        *rcsid = "$Id: $";
 #include <mntent.h>
 #include <sys/soundcard.h>
 #include <linux/cdrom.h>
+#include <linux/ucdrom.h>
 #else
 #include <sys/mnttab.h>
 #include <sys/cdio.h>
@@ -40,71 +44,165 @@ static char        *rcsid = "$Id: $";
 #include "xcdplay.h"
 #include "string.h"
 #include "player.h"
-    
+
+/*
+ * Set the drive.
+ */
+int
+Player::setdevice(char *devname) {
+    if (name != devname) {
+	if (device > 0)
+	    close(device);
+	device = -1;
+	name = devname;
+        if (!opendrive())
+           return MAXDRIVES;
+    }
+#if MAXDRIVES > 1
+    changing = 1;
+    for (int i = 0; i < MAXDRIVES; i++ ) {
+       if (ioctl(device, CDROM_SELECT_DISC, i)== 0) {
+            readtoc(&info[i]);
+	    continue;
+       }
+      /* Linux is broken here, could be either empty Slot or No Slot */
+       if (errno == ENOENT)
+	    continue;
+       changing = 0;
+      /* Check if maybe not a changer */
+       if (errno == EINVAL) 
+	    return (i > 0) ? (i+1) : 1;
+       {
+	    char buffer[50];
+
+	    sprintf(buffer, "Unable to access slot %d", i + 1);
+	    app_error(buffer);
+       }
+       return i;
+   }
+   changing = 0;
+#else
+   readtoc(&info[0]);
+#endif
+   return MAXDRIVES;
+}
+
+
 /*
  * Read in the table of contents off the cdrom.
  */
 int
-Player::readtoc()
+Player::readtoc(driveinfo *ip)
 {
     struct cdrom_tochdr	hdr;
     struct cdrom_tocentry	entry;
+    struct rawtrackinfo *rp;
     int i, pos;
     
     if (device < 0)
         return 1;
-    numtracks = -1;
-    length = -1;
+    ip->numtracks = -1;
+    ip->length = -1;
     if (ioctl(device, CDROMREADTOCHDR, &hdr) < 0) {
         app_error("Unable to read TOC");
         return 1;
     }
-    length = 0;
-    numtracks = hdr.cdth_trk1;
+    ip->length = 0;
+    ip->numtracks = hdr.cdth_trk1;
 
-    delete [] tracks;
-    tracks = new rawtrackinfo[numtracks+1];
+    delete [] ip->tracks;
+    rp = ip->tracks = new rawtrackinfo[ip->numtracks+1];
 
-    for (i=0; i <= numtracks; i++) {
-        entry.cdte_track = (i == numtracks)?CDROM_LEADOUT:(i+1);
+    for (i=0; i <= ip->numtracks; i++, rp++) {
+        entry.cdte_track = (i == ip->numtracks)?CDROM_LEADOUT:(i+1);
         entry.cdte_format = CDROM_MSF;
         if (ioctl(device, CDROMREADTOCENTRY, &entry) < 0) {
             app_error("Unable to read trackdata");
-            numtracks = -1;
-            length = -1;
+            ip->numtracks = -1;
+            ip->length = -1;
             return 1;
         }
-        tracks[i].data = entry.cdte_ctrl & CDROM_DATA_TRACK ? 1 : 0;
-        tracks[i].length = entry.cdte_addr.msf.minute * 60 +
+        rp->data = entry.cdte_ctrl & CDROM_DATA_TRACK ? 1 : 0;
+        rp->length = entry.cdte_addr.msf.minute * 60 +
               entry.cdte_addr.msf.second;
-        tracks[i].start = tracks[i].length * 75 +
-               entry.cdte_addr.msf.frame;
-        tracks[i].track = i + 1;
+        rp->start = rp->length * 75 + entry.cdte_addr.msf.frame;
+        rp->track = i + 1;
     }
 
    /* Set positions correctly */
-    pos = tracks[0].length;
-    for(i = 0; i < numtracks; i++) {
-        tracks[i].length = tracks[i+1].length - pos;
-        pos = tracks[i+1].length;
+    pos = ip->tracks[0].length;
+    rp = ip->tracks;
+    for(i = 0; i < ip->numtracks; i++, rp++) {
+        rp->length = rp[1].length - pos;
+        pos = rp[1].length;
     }
 
-    length = tracks[numtracks].length;
+    ip->length = ip->tracks[ip->numtracks].length;
 
    /* Compute disc id. Make sure same as all other CD programs */
     pos = 0;
-    id = 0;
-    for (i = 0; i < numtracks; i++) {
+    ip->id = 0;
+    rp = ip->tracks;
+    for (i = 0; i < ip->numtracks; i++, rp++) {
         char	buf[12], *p;
         int	sum = 0;
 
-        sprintf(buf, "%lu", tracks[i].start / 75);
+        sprintf(buf, "%lu", rp->start / 75);
         for(p = buf; *p != '\0'; sum += *p++ - '0');
         pos += sum;
-        id += tracks[i].length;
+        ip->id += rp->length;
     }
-    id = ((pos % 0xff) << 24) | (id << 8) | numtracks;
+    ip->id = ((pos % 0xff) << 24) | (ip->id << 8) | ip->numtracks;
     return 0;
+}
+
+/*
+ * Open the drive.
+ */
+int
+Player::opendrive()
+{
+    char 	*p;
+
+    curmode = Eject;
+
+   /* If name is a list try each one */
+    if ((p = strchr(name.cstring(), ':')) != NULL) {
+        char   *p1 = new char [strlen(name.cstring())+1];
+        strcpy(p1, name.cstring());
+        while(*p1 != '\0') {
+            if((p = strchr(p1, ':')) == NULL)
+                *p++ ='\0';
+            else
+                p = &p1[strlen(p1)];
+            if ((device = open(p1, O_RDONLY)) >= 0) {
+                curmode = Empty;
+                break;
+            }
+            p1 = p;
+        }
+        return 0;
+    } else {
+        if ((device = open(name.cstring(), O_RDONLY)) < 0) {
+            switch (errno) {
+#ifndef sun
+            case ENOENT:
+#endif
+            case EACCES:
+            case ENXIO:
+                app_fatal("Unable to open CDROM");
+#ifdef sun
+            case ENOENT:
+#endif
+                break;
+            default:
+                curmode = Mount;
+            }
+            device = -1;
+            return 0;
+        }
+    }
+   return 1;
 }
 
 /*
@@ -119,48 +217,35 @@ Player::checkstatus()
     frame = -1;
 
    /* If the device is not opened. Try to open it */
-    if (device < 0) {
-        char 	*p;
+    if (device < 0 && !opendrive()) 
+	return 0;
 
-        curmode = Eject;
+    if (changing)
+	return 1;
+#ifdef linux
+    if (ioctl(device, CDROM_MEDIA_CHANGED, 0) > 0) {
+	fprintf(stderr, "Media Changed\n");
+	curmode = Empty;
+    }
+#endif
 
-   /* If name is a list try each one */
-        if ((p = strchr(name.cstring(), ':')) != NULL) {
-            char   *p1 = new char [strlen(name.cstring())+1];
-            strcpy(p1, name.cstring());
-            while(*p1 != '\0') {
-                if((p = strchr(p1, ':')) == NULL)
-                    *p++ ='\0';
-                else
-                    p = &p1[strlen(p1)];
-                if ((device = open(p1, O_RDONLY)) >= 0) {
-                    curmode = Empty;
-                    break;
-                }
-                p1 = p;
-            }
+   /* If no table of contents. Get one */
+    if (curmode == Empty || curmode == Eject) {	/* Cd ejected */
+    	for (int i = 0; i < MAXDRIVES; i++ ) 
+	    if (ioctl(device, CDROM_SELECT_DISC, i)== 0) 
+	        readtoc(&info[i]);
+	if (info[curdrive].numtracks == -1) {
+            curmode = Mount;
+            close(device);
+            device = -1;
             return 0;
-        } else {
-            if ((device = open(name.cstring(), O_RDONLY)) < 0) {
-                switch (errno) {
-#ifndef sun
-                case ENOENT:
-#endif
-                case EACCES:
-                case ENXIO:
-                    app_fatal("Unable to open CDROM");
-#ifdef sun
-                case ENOENT:
-#endif
-                    break;
-                default:
-                    curmode = Mount;
-                }
-                device = -1;
-                return 0;
-            }
         }
-        curmode = Empty;
+        ioctl(device, CDROM_SELECT_DISC, curdrive);
+        curmode = New;
+        curpos = 0;
+        curtrack = 1;
+        frame = 0;
+        return 1;
     }
 
    /* Try to read device status */
@@ -177,21 +262,7 @@ Player::checkstatus()
         return 0;
     }
 
-   /* If no table of contents. Get one */
-    if (curmode == Empty || curmode == Eject) {	/* Cd ejected */
-        if (readtoc()) {
-            curmode = Mount;
-            close(device);
-            device = -1;
-            return 0;
-        }
-        curmode = New;
-        curpos = 0;
-        curtrack = 1;
-        frame = 0;
-        return 1;
-    }
-
+    struct driveinfo *ip = &info[curdrive];
    /* Now update frame and track information */	
     switch (sub.cdsc_audiostatus) {
     case CDROM_AUDIO_PAUSED:
@@ -205,13 +276,13 @@ Player::checkstatus()
         curpos = sub.cdsc_absaddr.msf.minute * 60 +
                   sub.cdsc_absaddr.msf.second;
         frame = curpos * 75 + sub.cdsc_absaddr.msf.frame;
-        if (curtrack < 1 || frame < tracks[curtrack-1].start ||
-            frame >= (curtrack >= numtracks ? (length + 1) * 75:
-            tracks[curtrack].start)) {
-            for(curtrack = 0; curtrack <= numtracks &&
-                        frame >= tracks[curtrack].start; curtrack++);
+        if (curtrack < 1 || frame < ip->tracks[curtrack-1].start ||
+            frame >= (curtrack >= ip->numtracks ? (ip->length + 1) * 75:
+            ip->tracks[curtrack].start)) {
+            for(curtrack = 0; curtrack <= ip->numtracks &&
+                        frame >= ip->tracks[curtrack].start; curtrack++);
         }
-        if (curtrack >= 1 && sub.cdsc_trk > tracks[curtrack-1].track)
+        if (curtrack >= 1 && sub.cdsc_trk > ip->tracks[curtrack-1].track)
             curtrack++;
         break;
     case CDROM_AUDIO_COMPLETED:
@@ -226,6 +297,34 @@ Player::checkstatus()
         break;
     }
     return 1;
+}
+
+/* Change to a new device */
+int
+Player::setdrive(int num)
+{
+#if MAXDRIVES > 1
+    if (device < 0)
+	return 0;
+    changing = 1;
+    if (ioctl(device, CDROM_SELECT_DISC, num) == 0) {
+        readtoc(&info[num]);
+        curdrive = num;
+	changing = 0;
+	return 1;
+    } else {
+	if (errno != ENOENT && errno != EINVAL) {
+	    char buffer[50];
+
+	    sprintf(buffer, "Unable to change to slot %d", num + 1);
+	    app_error(buffer);
+	}
+    }
+    changing = 0;
+    return 0;
+#else
+    return 1;
+#endif
 }
 
 /* Pause cdrom */
@@ -285,7 +384,7 @@ Player::eject()
     device = -1;
 #endif
     curtrack = -1;
-    length = 1;
+    info[curdrive].length = 1;
     curpos = 0;
     curmode = Eject;
     return;
@@ -296,20 +395,21 @@ int
 Player::play_cd(int track, int start, int duration)
 {
     int spos, epos;
+    struct driveinfo *ip = &info[curdrive];
     struct cdrom_msf	msf;
 
     track--;
 
-    if (track < 0 || track >= numtracks || device < 0)
+    if (track < 0 || track >= ip->numtracks || device < 0)
         return -1;
-    if (tracks[track].data)
+    if (ip->tracks[track].data)
         return -2;
 
-    spos = tracks[track].start + start * 75;
+    spos = ip->tracks[track].start + start * 75;
     if (duration == 0)
-        epos = tracks[track+1].start - 1;
+        epos = ip->tracks[track+1].start - 1;
     else
-        epos = tracks[track].start + duration * 75 - 1;
+        epos = ip->tracks[track].start + duration * 75 - 1;
     msf.cdmsf_min0 = spos / (60*75);
     msf.cdmsf_sec0 = (spos / 75) % 60;
     msf.cdmsf_frame0 = spos % 75; 
@@ -407,9 +507,9 @@ Cdmode
 Player::getposition(int &track, int &trktime)
 {
     if(checkstatus()) {
-        if (curtrack > 0 && curtrack <= numtracks)  {
+        if (curtrack > 0 && curtrack <= info[curdrive].numtracks)  {
             track = curtrack;
-            trktime = (frame - tracks[track - 1].start)/75;
+            trktime = (frame - info[curdrive].tracks[track - 1].start)/75;
         } else {
             track = 1;
             trktime = 0;

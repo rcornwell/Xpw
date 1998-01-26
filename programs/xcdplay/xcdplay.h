fb7bb7b697efd10cb8fb80dd129f814a2179ef18
Richard Cornwell <rcornwell@github.com>
1998-01-26 01:02:02 +0000

    Support for hiding clues.

--- programs/xcdplay/xcdplay.h
+++ programs/xcdplay/xcdplay.h
@@ -1,11 +1,16 @@
 /*
  * XCdplay: Plays a cd.
  *
- * $Id: $
+ * $Id: xcdplay.h,v 1.1 1997/12/16 05:48:46 rich Exp rich $
  *
- * $Log: $
+ * $Log: xcdplay.h,v $
+ * Revision 1.1  1997/12/16 05:48:46  rich
+ * Initial revision
+ *
  *
  */
+
+#define MAXDRIVES	4
 
 typedef struct _XCdplayResources {
     String		rcpath;
@@ -21,6 +26,7 @@ typedef struct _XCdplayResources {
     Boolean		show_track;
     Boolean		show_infobar;
     Boolean		autoplay;
+    Boolean		show_clues;
     int                 poll_rate;
     int			stop_rate;
     int			empty_rate;
@@ -42,6 +48,7 @@ extern DefRes *default_resources;
 extern void app_quit(Widget w, XtPointer client_data, XtPointer call_data);
 extern void app_error(char *msg);
 extern void app_fatal(char *msg);
+extern void checkdb();
 extern char *ExpandPath(char *path);
 extern void ReadRCFile();
 extern void WriteRCFile();
